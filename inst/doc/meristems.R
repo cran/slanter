ced4b0d0069527f3cf2093b78b4605419844d529
Oren Ben-Kiki <oren@ben-kiki.org>
2021-05-09 07:10:02 +0000

    version 0.2-0

--- inst/doc/meristems.R
+++ inst/doc/meristems.R
@@ -1,27 +1,27 @@
-## ----setup, include=FALSE------------------------------------------------
+## ----setup, include=FALSE-----------------------------------------------------
 knitr::opts_chunk$set(collapse=TRUE, comment="#>")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 library('slanter')
 data('meristems')
 similarity <- meristems
 similarity[similarity < 0] <- 0
 
-## ---- fig.show='hold'----------------------------------------------------
+## ---- fig.show='hold'---------------------------------------------------------
 pheatmap::pheatmap(meristems, show_rownames=FALSE, show_colnames=FALSE)
 
-## ---- fig.show='hold'----------------------------------------------------
+## ---- fig.show='hold'---------------------------------------------------------
 pheatmap::pheatmap(meristems, show_rownames=FALSE, show_colnames=FALSE, cluster_rows=FALSE, cluster_cols=FALSE)
 
-## ---- fig.show='hold'----------------------------------------------------
+## ---- fig.show='hold'---------------------------------------------------------
 sheatmap(meristems, order_data=similarity, show_rownames=FALSE, show_colnames=FALSE, cluster_rows=FALSE, cluster_cols=FALSE)
 
-## ---- fig.show='hold'----------------------------------------------------
+## ---- fig.show='hold'---------------------------------------------------------
 sheatmap(meristems, order_data=similarity, show_rownames=FALSE, show_colnames=FALSE, oclust_rows=FALSE, oclust_cols=FALSE)
 
-## ---- fig.show='hold'----------------------------------------------------
+## ---- fig.show='hold'---------------------------------------------------------
 sheatmap(meristems, order_data=similarity, show_rownames=FALSE, show_colnames=FALSE)
 
-## ---- fig.show='hold'----------------------------------------------------
+## ---- fig.show='hold'---------------------------------------------------------
 sheatmap(meristems, order_data=similarity, show_rownames=FALSE, show_colnames=FALSE, cutree_rows=5, cutree_cols=5)
 
